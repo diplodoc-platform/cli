b751929afc482c5edfad4993b0ad2d3572016dae
Natalia Novikova <novikovans94@gmail.com>
2021-07-01 16:14:51 +0300

    Comments were fixed

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,19 +1,19 @@
 import {Platforms} from '../constants';
 
-const windowsSlash = '\\';
-const linuxSlash = '/';
+const backSlash = '\\';
+const slash = '/';
 
 export function addSlashPrefix(path: string): string {
     const slashPrefix = process.platform === Platforms.WINDOWS
-        ? path.startsWith(windowsSlash) ? '' : windowsSlash
-        : path.startsWith(linuxSlash) ? '' : linuxSlash;
+        ? path.startsWith(backSlash) ? '' : backSlash
+        : path.startsWith(slash) ? '' : slash;
 
     return `${slashPrefix}${path}`;
 }
 
 export function convertBackSlashToSlash(path: string): string {
     if (process.platform === Platforms.WINDOWS) {
-        return path.replace(/\\/g, linuxSlash);
+        return path.replace(/\\/g, slash);
     }
 
     return path;
