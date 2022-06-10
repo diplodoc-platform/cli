984a2094c57b22802942550b8e0bcec9b54dfd2e
martyanov-av <martyanov-av@yandex-team.ru>
2022-06-10 13:07:24 +0300

    fix: use TocService to generate map file

--- src/steps/processMapFile.ts
+++ src/steps/processMapFile.ts
@@ -1,10 +1,7 @@
-import {readFileSync, writeFileSync} from 'fs';
-import {dirname, extname, join} from 'path';
-import yaml from 'js-yaml';
-import walkSync from 'walk-sync';
+import {writeFileSync} from 'fs';
+import {extname, join} from 'path';
 
-import {ArgvService} from '../services';
-import {isExternalHref} from '../utils';
+import {ArgvService, TocService} from '../services';
 
 type TocItem = {
     name: string;
@@ -14,54 +11,24 @@ type TocItem = {
 
 type TocItems = TocItem[];
 
-type Toc = {
-    title: string;
-    items: TocItems;
-};
-
 export function prepareMapFile(): void {
     const {
         output: outputFolderPath,
     } = ArgvService.getConfig();
 
-    const navigationPaths = {
-        files: collectFiles(outputFolderPath),
-    };
+    const navigationPathsWithoutExtensions =
+        TocService.getNavigationPaths().map((path) => {
+            let preparedPath = path.replace(extname(path), '');
 
+            if (preparedPath.endsWith('/index')) {
+                preparedPath = preparedPath.substring(0, preparedPath.length - 5);
+            }
+
+            return preparedPath;
+        });
+    const navigationPaths = {files: [...new Set(navigationPathsWithoutExtensions)]};
     const filesMapBuffer = Buffer.from(JSON.stringify(navigationPaths, null, '\t'), 'utf8');
     const mapFile = join(outputFolderPath, 'files.json');
 
     writeFileSync(mapFile, filesMapBuffer);
-}
-
-function walkItems(items: TocItems, source: string) {
-    return items.reduce((acc: string[], {href, items: subItems}) => {
-        if (href && !isExternalHref(href)) {
-            acc.push(join(source, href).replace(extname(href), ''));
-        }
-
-        if (subItems) {
-            acc.push(...walkItems(subItems, source));
-        }
-
-        return acc;
-    }, []);
-}
-
-function collectFiles(cwd: string): string[] {
-    const files = walkSync(cwd, {
-        directories: false,
-        globs: ['**/toc.yaml'],
-    });
-
-    return files
-        .filter((path) => !path.includes('/_'))
-        .reduce((acc: string[], path: string) => {
-            const fullPath = join(cwd, path);
-            const toc = yaml.load(readFileSync(fullPath, 'utf8')) as Toc;
-
-            acc.push(`${dirname(path)}/`);
-
-            return acc.concat(walkItems(toc.items, dirname(path)));
-        }, []);
 }
