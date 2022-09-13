2681cbb437328e180e80f1c2186e881685647c56
moki-codes <moki-codes@yandex-team.ru>
2022-09-13 03:04:20 +0300

    feat(cmd/xliff/compose): compose xlf and skl into documentation
    compose xliff and skeleton files into documentation
    
    skeleton files are original markdown files with text tokens replaced
    with hashes.
    
    xliff files contain aforementioned text tokens from the documentation
    with translated by you text tokens next to them.
    
    compose them into documentation, giving you translated document.

--- src/cmd/xliff/compose.ts
+++ src/cmd/xliff/compose.ts
@@ -1,4 +1,13 @@
 import {Arguments} from 'yargs';
+const {promises: {readFile, writeFile, mkdir}} = require('fs');
+import {resolve, dirname} from 'path';
+
+const yfm2xliff = require('@doc-tools/yfm2xliff/lib/cjs');
+
+import {deepFiles} from '../../utils';
+import {
+    allPass, complement, isHidden,
+} from '../../services/includers/batteries/common';
 
 const command = 'compose';
 
@@ -6,11 +15,44 @@ const description = 'compose xliff and skeleton into documentation';
 
 const compose = {command, description, handler};
 
-function handler(args: Arguments<any>) {
-    console.log('handling xliff compose');
-    console.log('xliff compose', JSON.stringify(args, null, 4));
+const isSKLExtension = (str: string) => /.skl.md$/gmu.test(str);
+const filterSKL = allPass([isSKLExtension, complement(isHidden)]);
+const deepSKLFiles = deepFiles(filterSKL);
 
-    throw new Error('not implemented');
+const isXLFExtension = (str: string) => /.xlf$/gmu.test(str);
+const filterXLF = allPass([isXLFExtension, complement(isHidden)]);
+const deepXLFFiles = deepFiles(filterXLF);
+
+const composer = async (xliff: string, skeleton: string) => new Promise((res, rej) =>
+    yfm2xliff.compose(xliff, skeleton, (err: Error, composed: string) => {
+        if (err) {
+            rej(err);
+        }
+
+        return res(composed);
+    }));
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+async function handler(args: Arguments<any>) {
+    const {input, output} = args;
+
+    const [sklPaths, xlfPaths] = await Promise.all([
+        deepSKLFiles(input),
+        deepXLFFiles(input)]);
+
+    const [skls, xlfs, paths] = await Promise.all([
+        Promise.all(sklPaths.map(async (path: string) =>
+            readFile(resolve(path), {encoding: 'utf-8'}))),
+        Promise.all(xlfPaths.map(async (path: string) =>
+            readFile(resolve(path), {encoding: 'utf-8'}))),
+        Promise.all(xlfPaths.map(async (path: string) =>
+            path.replace(/.xlf$/gmu, '').replace(input, output)))]);
+
+    await Promise.all(paths.map(async (path, i) => {
+        await mkdir(dirname(path), {recursive: true});
+
+        return writeFile(`${path}.md`, await composer(xlfs[i], skls[i]));
+    }));
 }
 
 export {compose};
