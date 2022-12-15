709a9c8833e1c9165016bb1eebed890020908b12
moki-codes <moki-codes@yandex-team.ru>
2022-12-15 19:11:39 +0400

    feat(services/includers)!: new architecture
    BREAKING CHANGE: includers now have new toc interface

--- src/services/includers/index.ts
+++ src/services/includers/index.ts
@@ -1,30 +1,194 @@
-import {sourcedocs, openapi} from './batteries';
-import {Includer, YfmTocInclude} from '../../models';
+import {join} from 'path';
 
-type IncludersMap = {[key: string]: Includer};
+import {isObject} from 'lodash';
+
+import {ArgvService} from '../index';
+import {IncludeMode} from '../../constants';
+import {logger} from '../../utils/logger';
+
+import type {
+    Includer,
+    YfmToc,
+    YfmTocInclude,
+    YfmTocIncluder,
+    YfmTocIncluders,
+    YfmTocIncluderObject,
+    YfmTocIncludersNormalized,
+} from '../../models';
+
+const includerUsage = `include:
+  path: <path-where-to-include>
+  includer:
+    name: <includer-name>
+    <includer-parameter>: <value-for-includer-parameter>
+`;
+
+const includersUsage = `include:
+  path: <path-where-to-include>
+  includers:
+    - name: <includer-name-0>
+      <includer-parameter>: <value-for-includer-parameter>
+    - name: <includer-name-1>
+      <includer-parameter>: <value-for-includer-parameter>
+`;
+
+type IncludersMap = Record<string, Includer>;
 
 let includersMap!: IncludersMap;
+
+class IncludersError extends Error {
+    path: string;
+
+    constructor(message: string, path: string) {
+        super(message);
+
+        this.name = 'IncludersError';
+        this.path = path;
+    }
+}
 
 function init(custom: Includer[] = []) {
     if (includersMap) { return; }
 
-    includersMap = {
-        sourcedocs,
-        openapi,
-    };
+    includersMap = {};
 
     for (const includer of custom) {
         includersMap[includer.name] = includer;
     }
 }
 
-const isValidIncluder = (include: YfmTocInclude) =>
-        include?.path?.length && include?.includer && includersMap[include.includer as keyof typeof includersMap];
+async function applyIncluders(path: string, item: YfmToc) {
+    if (!(item && item.include) || !includeHasIncluders(item.include)) {
+        return;
+    }
 
-const getIncluder = (include: YfmTocInclude) => {
-    if (isValidIncluder(include)) { return includersMap[include.includer as keyof typeof includersMap]; }
+    if (!includeValid(item.include)) {
+        throw new IncludersError('include doesn\'t comply with includers standard', path);
+    }
 
-    throw new Error('includer not implemented');
+    // normalize include mode (includers support link mode only)
+    item.include.mode = IncludeMode.LINK;
+
+    const includers = normalizeIncludeIncluders(path, item.include) as YfmTocIncludersNormalized;
+
+    item.include.includers = includers;
+
+    for (const {name, ...rest} of includers) {
+        const includer = getIncluder(name);
+
+        await applyIncluder({path, item, includer, passedParams: rest});
+    }
+
+    // contract to be fullfilled by the includer:
+    // provide builder generated toc.yaml
+    item.include.path = join(item.include.path, 'toc.yaml');
+}
+
+function includeHasIncluders(include: YfmTocInclude) {
+    return include.includer || include.includers;
+}
+
+function includeValid(include: YfmTocInclude) {
+    return (include.mode === IncludeMode.LINK || !include.mode) && include.path?.length;
+}
+
+function normalizeIncludeIncluders(path: string, include: YfmTocInclude) {
+    if (include.includers) {
+        const {status, message} = includersValid(include.includers);
+
+        if (status) {
+            return include.includers;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        throw new IncludersError(message!, path);
+    }
+
+    logger.warn(path, 'includer field is getting depricated, use includers field\n' + includersUsage);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const {status, message} = includerValid(include.includer!);
+
+    if (status) {
+        return [include.includer];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    throw new IncludersError(message!, path);
+}
+
+function includersValid(includers: YfmTocIncluders) {
+    for (const includer of includers) {
+        const {status, message} = includerValid(includer);
+
+        if (!status) {
+            return {status, message};
+        }
+    }
+
+    return {status: true};
+}
+
+function includerValid(includer: YfmTocIncluder) {
+    if (Array.isArray(includer)) {
+        return {status: false, message: `use includers field to provide multiple includers:\n${includersUsage}`};
+    }
+
+    if (typeof includer === 'string') {
+        return {status: false, message: `use updated includer format:\n${includerUsage}`};
+    }
+
+    if (isObject(includer)) {
+        if (typeof includer.name !== 'string') {
+            return {
+                status: false,
+                message: 'use string to specify includers name',
+            };
+        }
+
+        if (includerExists(includer)) {
+            return {status: true};
+        }
+
+        return {status: false, message: `includer ${includer.name} not implemented`};
+    }
+
+    return {
+        status: false,
+        message: `use appropriate includer/includers format:\n${includerUsage}${includersUsage}`,
+    };
+}
+
+function getIncluder(includerName: string) {
+    return includersMap[includerName];
+}
+
+function includerExists(includer: YfmTocIncluderObject) {
+    return includersMap[includer.name as keyof typeof includersMap];
+}
+
+export type applyIncluderParams = {
+    path: string;
+    item: YfmToc;
+    includer: Includer;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    passedParams: Record<string, any>;
 };
 
-export {init, isValidIncluder, getIncluder};
+async function applyIncluder(args: applyIncluderParams) {
+    const {rootInput: readBasePath, input: writeBasePath} = ArgvService.getConfig();
+
+    const {path, item, includer, passedParams} = args;
+
+    const params = {
+        passedParams,
+        item,
+        readBasePath,
+        writeBasePath,
+        tocPath: path,
+    };
+
+    await includer.includerFunction(params);
+}
+
+export {init, applyIncluders, IncludersError};
