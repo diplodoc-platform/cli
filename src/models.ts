7e96e2636c6d2d0cf8d4d008a20f7b5c6480df8b
moki-codes <moki-codes@yandex-team.ru>
2022-12-17 20:55:48 +0400

    feat(includers/sourcedoc): rewrite

--- src/models.ts
+++ src/models.ts
@@ -87,7 +87,7 @@ export type YfmTocIncluders = YfmTocIncluder[];
 
 export type YfmTocIncluder = YfmTocIncluderName | YfmTocIncluderObject;
 
-export const includersNames = [] as const;
+export const includersNames = ['sourcedocs'] as const;
 
 export type YfmTocIncluderName = typeof includersNames[number];
 
