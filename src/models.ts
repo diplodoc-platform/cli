e014393a24282a57c11d8f0ac4cc38d6fdf91649
Kirill Omelianenko <kiri111enz@kiri111enz-dev.vla.yp-c.yandex.net>
2023-12-05 15:28:32 +0300

    fix: build only used langs

--- src/models.ts
+++ src/models.ts
@@ -62,6 +62,7 @@ export interface YfmArgv extends YfmConfig {
     addMapFile: boolean;
     allowCustomResources: boolean;
     staticContent: boolean;
+    langs: string[];
 }
 
 export interface DocPreset {
