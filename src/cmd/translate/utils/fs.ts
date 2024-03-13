4b3711bd8efd5f95ce546f1462ff4ad2c5592664
3y3 <3y3@ya.ru>
2024-03-13 19:16:23 +0300

    feat(translation): Add schemas for toc/leading/presets

--- src/cmd/translate/utils/fs.ts
+++ src/cmd/translate/utils/fs.ts
@@ -1,17 +1,30 @@
 import {JSONValue, resolveRefs} from '@diplodoc/translation';
-import {extname} from 'path';
 import {dump, load} from 'js-yaml';
 import {readFile, writeFile} from 'fs/promises';
+
+function last<T>(array: T[]): T | undefined {
+    return array[array.length - 1];
+}
+
+function ext(path: string) {
+    const parts = path.split('.');
+
+    if (last(parts) === 'skl') {
+        parts.pop();
+    }
+
+    return last(parts);
+}
 
 function parseFile(text: string, path: string): JSONValue | string {
     if (typeof text !== 'string') {
         return text;
     }
 
-    switch (extname(path)) {
-        case '.yaml':
+    switch (ext(path)) {
+        case 'yaml':
             return load(text) as object;
-        case '.json':
+        case 'json':
             return JSON.parse(text);
         default:
             return text;
@@ -23,22 +36,22 @@ function stringifyFile(content: JSONValue | string, path: string): string {
         return content;
     }
 
-    switch (extname(path)) {
-        case '.yaml':
+    switch (ext(path)) {
+        case 'yaml':
             return dump(content);
-        case '.json':
+        case 'json':
             return JSON.stringify(content);
         default:
             return content as unknown as string;
     }
 }
 
-export async function loadFile<T = string | JSONValue>(path: string): Promise<T> {
+export async function loadFile<T = string | JSONValue>(path: string, resolve = true): Promise<T> {
     const text = await readFile(path, 'utf8');
 
     let content = parseFile(text, path);
 
-    if (content && typeof content === 'object') {
+    if (content && typeof content === 'object' && resolve) {
         content = await resolveRefs(content, path, parseFile);
     }
 
@@ -49,4 +62,28 @@ export async function dumpFile(path: string, content: string | JSONValue) {
     const text = stringifyFile(content, path);
 
     await writeFile(path, text, 'utf8');
+}
+
+/**
+ * Takes toc schema if file matched as toc.
+ * Takes leading schema if file matched as leading page.
+ * Takes presets schema if file matched as presets.
+ * Any way translation inner logic will search `$schema` attribute with high priority.
+ * If `$schema` attribute not found anc precise schema not resolved,
+ * we think that current yaml is a part of complex toc.yaml
+ */
+export async function resolveSchemas(path: string) {
+    if (path.endsWith('toc.yaml')) {
+        return [await loadFile('schemas/toc-schema.yaml', false)];
+    }
+
+    if (path.endsWith('index.yaml')) {
+        return [await loadFile('schemas/leading-schema.yaml', false)];
+    }
+
+    if (path.endsWith('presets.yaml')) {
+        return [await loadFile('schemas/presets-schema.yaml', false)];
+    }
+
+    return [await loadFile('schemas/toc-schema.yaml', false)];
 }
