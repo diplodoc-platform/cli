5612b0205d410d917121547e8647f034ca640b63
Maxim Karpov <makame@mail.ru>
2024-04-01 18:24:12 +0300

    fix: remove meta fixRelativePath

--- src/resolvers/md2html.ts
+++ src/resolvers/md2html.ts
@@ -30,10 +30,6 @@ const FileTransformer: Record<string, Function> = {
     '.md': MdFileTransformer,
 };
 
-const fixRelativePath = (relativeTo: string) => (path: string) => {
-    return join(getAssetsPublicPath(relativeTo), path);
-};
-
 const getFileMeta = async ({fileExtension, metadata, inputPath}: ResolverOptions) => {
     const {input, allowCustomResources} = ArgvService.getConfig();
 
@@ -57,10 +53,9 @@ const getFileMeta = async ({fileExtension, metadata, inputPath}: ResolverOptions
 
     if (allowCustomResources) {
         const {script, style} = metadata?.resources || {};
-        fileMeta.style = (fileMeta.style || []).concat(style || []).map(fixRelativePath(inputPath));
+        fileMeta.style = (fileMeta.style || []).concat(style || []);
         fileMeta.script = (fileMeta.script || [])
-            .concat(script || [])
-            .map(fixRelativePath(inputPath));
+            .concat(script || []);
     } else {
         fileMeta.style = [];
         fileMeta.script = [];
