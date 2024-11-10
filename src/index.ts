ef48e9e9f28b20bf88f63b4f1f285ad336464a74
3y3 <3y3@ya.ru>
2024-11-10 07:01:47 +0300

    chore: Drop useless `deprecated` helper

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export type {ICallable, IProgram, ProgramConfig, ProgramArgs} from './program';
 export {Program} from './program';
 
 export type {Config, OptionInfo} from './config';
-export {Command, option, deprecated} from './config';
+export {Command, option} from './config';
 
 import {Program} from './program';
 
