05669b070567ba03a3373b764c413549b194cfd3
3y3 <3y3@ya.ru>
2024-12-07 02:03:57 +0300

    chore: Persist files.json output

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = {
   transform: {
-    '^.+\\.(j|t)s?$': ['ts-jest', {tsconfig: '<rootDir>/tsconfig.json'}],
+    '^.+\\.ts?$': ['ts-jest', {tsconfig: '<rootDir>/tsconfig.json'}],
   },
   verbose: true,
   preset: 'ts-jest',
   testMatch: ['<rootDir>/**/*.@(test|spec).ts'],
   testEnvironment: 'node',
   roots: ['<rootDir>/..', '../src', '../tests'],
-  collectCoverageFrom: ['../src/**/*.ts', '!../src/app/**', '!../src/**/*.d.ts'],
+  collectCoverageFrom: ['../src/**/*.ts', '!../src/**/*.d.ts'],
   coverageProvider: 'v8',
   coverageDirectory: '<rootDir>/coverage',
   moduleDirectories: ['node_modules', path.join(__dirname, '../src'), path.join(__dirname, '../tests')],
