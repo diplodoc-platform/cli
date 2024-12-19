7ef6c75eff5aaaf3fd233d28a9a75855a7758400
3y3 <3y3@ya.ru>
2024-12-19 19:58:18 +0300

    fix: Drop legacy toc service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,3 @@
-export {default as TocService} from './tocs';
 export {default as PresetService} from './preset';
 export {default as ArgvService} from './argv';
 export {default as LeadingService} from './leading';
