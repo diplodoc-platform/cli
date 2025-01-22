051d61a86537aa970a2416fe791b633ecce32b79
3y3 <3y3@ya.ru>
2025-01-22 17:00:43 +0300

    chore: Update tests config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -12,9 +12,7 @@ export default defineConfig({
             provider: 'v8',
             include: [
                 'src/commands',
-                'src/program',
-                'src/config',
-                'src/logger',
+                'src/core',
             ],
             exclude: [
                 'assets/**',
