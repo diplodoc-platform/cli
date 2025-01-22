16c4e8e65645af7be6f8fd97cdefa4367cac0e75
Roman Preslitsky <preslitsky@yandex-team.ru>
2025-01-22 19:15:55 +0300

    WIP themer

---
+++ src/steps/themer/constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_COLORS = {
+    // TODO add type
+    light: {
+        'base-background': 'rgb(255,255,255)',
+    },
+    dark: {
+        'base-background': 'rgb(45, 44, 51)', // То что у нас сейчас стоит
+    },
+};
