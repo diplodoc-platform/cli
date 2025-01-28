32da9576a50ef21fa77892e96ad10ec987cfc2b7
3y3 <3y3@ya.ru>
2025-01-28 18:34:15 +0300

    feat: Implement new Leading service

---
+++ src/core/leading/hooks.ts
@@ -0,0 +1,31 @@
+import type {LeadingPage, Plugin} from './types';
+
+import {AsyncParallelHook, AsyncSeriesHook, AsyncSeriesWaterfallHook} from 'tapable';
+
+import {generateHooksAccess} from '~/core/utils';
+
+export function hooks(name: string) {
+    return {
+        /**
+         * Emits relative to root asset path on each local link in Leading.
+         * This includes paths in links and blocks sections.
+         */
+        Asset: new AsyncParallelHook<[RelativePath, RelativePath]>(
+            ['asset', 'path'],
+            `${name}.Asset`,
+        ),
+        Plugins: new AsyncSeriesWaterfallHook<[Plugin[]]>(['plugins'], `${name}.Plugins`),
+        Resolved: new AsyncSeriesHook<[LeadingPage, RelativePath]>(
+            ['leading', 'path'],
+            `${name}.Resolved`,
+        ),
+        Dump: new AsyncSeriesWaterfallHook<[LeadingPage, NormalizedPath]>(
+            ['leading', 'path'],
+            `${name}.Dump`,
+        ),
+    };
+}
+
+const [getHooks, withHooks] = generateHooksAccess('Leading', hooks);
+
+export {getHooks, withHooks};
---
+++ src/core/leading/types.ts
@@ -0,0 +1,60 @@
+import type {Meta} from '~/core/meta';
+import type {LoaderContext} from './loader';
+
+// TODO: import this type from client?
+// import {PageContent} from '@gravity-ui/page-constructor';
+
+type PageContent = {
+    blocks: object;
+};
+
+export interface RawLeadingPage {
+    title: TextItems;
+    description?: TextItems;
+    meta?: {
+        title?: TextItems;
+        description?: TextItems;
+    } & Meta;
+    nav?: {
+        title: TextItems;
+    };
+    links: RawLeadingPageLink[];
+}
+
+export type TextItems = string | (TextItem | string)[];
+
+export interface TextItem extends Filter {
+    text: string | string[];
+}
+
+export interface RawLeadingPageLink extends Filter {
+    title?: TextItems;
+    description?: TextItems;
+    href?: string;
+}
+
+export interface Filter {
+    when?: boolean | string;
+    [key: string]: unknown;
+}
+
+export interface LeadingPage extends Partial<PageContent> {
+    title: string;
+    description?: string | string[];
+    meta?: Meta;
+    nav?: {
+        title: string;
+    };
+    links?: LeadingPageLink[];
+}
+
+export interface LeadingPageLink {
+    title?: string;
+    description?: string;
+    href?: string;
+}
+
+export type Plugin = (
+    this: LoaderContext,
+    leading: LeadingPage,
+) => LeadingPage | Promise<LeadingPage>;
