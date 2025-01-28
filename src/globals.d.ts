3ea5f4e4fe77b4f922f435234edcf93c1f50473e
3y3 <3y3@ya.ru>
2025-01-29 01:12:16 +0300

    chore: Remove useless types from globals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,8 +1,6 @@
 declare const __dirname: AbsolutePath;
 declare const require: Require;
 declare const VERSION: string;
-declare const OPENAPI_EXTENSION: string;
-declare const GENERIC_INCLUDER_EXTENSION: string;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Hash<T = any> = Record<string, T>;
