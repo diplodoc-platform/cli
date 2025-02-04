a754ce9a513b40ca6521a34c281c9ae3694d9c6b
Roman Preslitsky <preslitsky@yandex-team.ru>
2025-02-04 21:17:41 +0300

    fixes

--- src/steps/processThemer.ts
+++ src/steps/processThemer.ts
@@ -2,6 +2,7 @@ import {resolve} from 'node:path';
 import type {Run} from '~/commands/build';
 import {THEME_CSS_PATH} from '~/constants';
 import {
+    BRAND_COLOR_KEYS,
     BRAND_COLOR_VARIABLE_PREFIX,
     ColorsOptions,
     DEFAULT_BRAND_DEPEND_COLORS,
@@ -10,11 +11,13 @@ import {
     loadFile,
     Theme,
     THEME_GRAVITY_VARIABLE_PREFIX,
+    THEME_VARIANTS,
     THEME_YFM_VARIABLE_PREFIX,
     ThemeConfig,
     ThemeOptions,
     ThemeVariant,
     YFM_COLOR_KEYS,
+    YFMColorOptions,
 } from './themer';
 import {writeFileSync} from 'node:fs';
 
@@ -29,100 +32,161 @@ export async function processThemer(run: Run) {
             throw Error(validate.errors ? validate.errors[0].message : 'validation error');
         }
     } catch (e) {
-        console.log(e);
+        run.logger.warn('Theme config error');
+        run.logger.warn(e);
     }
 }
 
 function createTheme(configData: ThemeConfig): Theme {
-    return {
-        light: createVariant({configData, themeVariant: 'light'}),
-        dark: createVariant({configData, themeVariant: 'dark'}),
-    };
+    let theme: Theme = {};
+
+    // if (configData['base-background']) {
+    //     delete configData['base-background'];
+    // }
+
+    for (const key of BRAND_COLOR_KEYS) {
+        if (configData[key]) {
+            configData.light = configData.light ?? {};
+            configData.dark = configData.dark ?? {};
+            if (!configData.light[key]) {
+                configData.light[key] = configData[key];
+            }
+            if (!configData.dark[key]) {
+                configData.dark[key] = configData[key];
+            }
+        }
+    }
+    const hasBaseColors = Object.keys(configData).some((key) => key !== 'light' && key !== 'dark');
+
+    if (hasBaseColors) {
+        theme.base = createBaseVariant(configData);
+    }
+    if (configData.light) {
+        theme.light = createVariant(configData, 'light');
+    }
+    if (configData.dark) {
+        theme.dark = createVariant(configData, 'dark');
+    }
+
+    return theme;
 }
 
-function createVariant({
-    configData,
-    themeVariant,
-}: {
-    configData: ThemeConfig;
-    themeVariant: ThemeVariant;
-}): ThemeOptions {
-    const brandColor = configData[themeVariant]['base-brand'];
-    const backgroundColor = configData[themeVariant]['base-background'];
-    let themeOptions: ThemeOptions = {colors: {}, palette: {}};
+function createVariant(
+    configData: ThemeConfig,
+    themeVariant: ThemeVariant,
+): ThemeOptions | undefined {
+    if (!configData[themeVariant]) {
+        return;
+    }
+
+    const {'base-brand': brandColor, 'base-background': backgroundColor} = configData[themeVariant];
+    let themeOptions: ThemeOptions = {colors: {}};
 
     if (brandColor || backgroundColor) {
         themeOptions.colors = {
             ...DEFAULT_BRAND_DEPEND_COLORS[themeVariant],
         };
 
-        const opposedVariant = themeVariant === 'light' ? 'dark' : 'light';
-        const opossedBg =
-            configData[opposedVariant]['base-background'] ??
-            DEFAULT_BRAND_DEPEND_COLORS[opposedVariant]['base-background'];
+        const oppositeVariant = THEME_VARIANTS.find((v) => v !== themeVariant) as ThemeVariant;
+        const oppositeBg =
+            configData[oppositeVariant]?.['base-background'] ??
+            DEFAULT_BRAND_DEPEND_COLORS[oppositeVariant]['base-background'];
 
         themeOptions.palette = generateBrandShades({
             colorValue: brandColor ?? DEFAULT_BRAND_DEPEND_COLORS[themeVariant]['base-brand'],
             lightBg:
                 themeVariant === 'light'
-                    ? DEFAULT_BRAND_DEPEND_COLORS[opposedVariant]['base-background']
-                    : opossedBg,
+                    ? DEFAULT_BRAND_DEPEND_COLORS[oppositeVariant]['base-background']
+                    : oppositeBg,
             darkBg:
                 themeVariant === 'dark'
-                    ? DEFAULT_BRAND_DEPEND_COLORS[opposedVariant]['base-background']
-                    : opossedBg,
+                    ? DEFAULT_BRAND_DEPEND_COLORS[oppositeVariant]['base-background']
+                    : oppositeBg,
         });
     }
 
-    for (const [key, value] of Object.entries(configData[themeVariant])) {
-        themeOptions.colors[key as keyof ColorsOptions] = value;
-    }
+    themeOptions.colors = {
+        ...themeOptions.colors,
+        ...configData[themeVariant],
+    };
 
     return themeOptions;
+}
+
+function createBaseVariant(configData: ThemeConfig): ThemeOptions {
+    const baseColors: ColorsOptions = {};
+    for (const key in configData) {
+        if (!THEME_VARIANTS.includes(key as ThemeVariant)) {
+            const option = key as keyof ColorsOptions;
+            baseColors[option] = configData[option];
+        }
+    }
+    return {
+        colors: baseColors,
+    };
 }
 
 function createCSS(theme: Theme, folderPath: AbsolutePath) {
     const themePath = resolve(folderPath, THEME_CSS_PATH);
     let cssText = '';
+    cssText += prepareThemeVariables('base', theme);
     cssText += prepareThemeVariables('light', theme);
     cssText += prepareThemeVariables('dark', theme);
-
     writeFileSync(themePath, cssText);
 }
 
-function prepareThemeVariables(themeVariant: ThemeVariant, theme: Theme) {
-    const INDENTATION = '    ';
-    const mainSelector = `.g-root_theme_${themeVariant}`;
-    const yfmSelector = `${mainSelector} .yfm`;
+function prepareThemeVariables(variant: ThemeVariant | 'base', theme: Theme) {
+    if (!theme[variant]) return '';
 
-    const cssVariables: string[] = [];
-    const yfmCssVariables: string[] = [];
+    let css = '';
+    if (variant === 'base') {
+        const gravityColors = getGravityCSSColors(theme[variant].colors);
+        const yfmCssVariables = getYFMCSSColors(theme[variant].colors);
 
-    cssVariables.push(`${mainSelector} {`);
-
-    const pallete = Object.entries(theme[themeVariant].palette).map(
-        ([code, color]) => `${INDENTATION}${BRAND_COLOR_VARIABLE_PREFIX}-${code}: ${color};`,
-    );
-
-    cssVariables.push(...pallete);
-    cssVariables.push('\n');
-
-    Object.entries(theme[themeVariant].colors).forEach(([code, color]) => {
-        if (YFM_COLOR_KEYS.includes(code)) {
-            yfmCssVariables.push(`${INDENTATION}${THEME_YFM_VARIABLE_PREFIX}-${code}: ${color};`);
-        } else {
-            cssVariables.push(`${INDENTATION}${THEME_GRAVITY_VARIABLE_PREFIX}-${code}: ${color};`);
+        css += gravityColors ? `.g-root {\n${gravityColors}\n}\n\n` : '';
+        css += yfmCssVariables ? `.yfm {\n${yfmCssVariables}\n}\n\n` : '';
+    } else {
+        let palette = '';
+        if (theme[variant].palette) {
+            palette = getPaletteCSSColors(theme[variant].palette);
         }
-    });
+        const gravityColors = getGravityCSSColors(theme[variant].colors);
+        const yfmCssVariables = getYFMCSSColors(theme[variant].colors);
 
-    cssVariables.push('}\n');
-
-    if (yfmCssVariables.length > 0) {
-        cssVariables.push(`${yfmSelector} {`);
-        cssVariables.push(...yfmCssVariables);
-        cssVariables.push('}');
+        if (palette != '' || gravityColors) {
+            css += `.g-root_theme_${variant} {\n${palette}\n${gravityColors}\n}\n\n`;
+        }
+        css += yfmCssVariables ? `.g-root_theme_${variant} .yfm {\n${yfmCssVariables}\n}\n\n` : '';
     }
+    return css;
+}
 
-    cssVariables.push('\n');
-    return cssVariables.join('\n');
+function getGravityCSSColors(colors: ColorsOptions): string | undefined {
+    if (!colors) return;
+
+    const css = Object.entries(colors)
+        .filter(([key]) => !YFM_COLOR_KEYS.includes(key as keyof YFMColorOptions))
+        .map(([key, value]) => `    ${THEME_GRAVITY_VARIABLE_PREFIX}-${key}: ${value};`)
+        .join('\n');
+
+    return css;
+}
+
+function getYFMCSSColors(colors: ColorsOptions): string | undefined {
+    if (!colors) return;
+
+    const css = Object.entries(colors)
+        .filter(([key]) => YFM_COLOR_KEYS.includes(key as keyof YFMColorOptions))
+        .map(([key, value]) => `    ${THEME_YFM_VARIABLE_PREFIX}-${key}: ${value};`)
+        .join('\n');
+
+    return css;
+}
+
+function getPaletteCSSColors(palette: Record<string, string>): string {
+    const css = Object.entries(palette)
+        .map(([key, value]) => `    ${BRAND_COLOR_VARIABLE_PREFIX}-${key}: ${value};`)
+        .join('\n');
+
+    return css;
 }
--- src/steps/themer/types.ts
+++ src/steps/themer/types.ts
@@ -2,23 +2,27 @@ import Record from '@diplodoc/client/manifest';
 
 export type ThemeVariant = 'light' | 'dark';
 
-export interface ThemeConfig extends Record<ThemeVariant, ColorsOptions> {}
+export type ThemeConfig = {
+    [K in keyof ColorsOptions]?: string;
+} & {
+    light?: ColorsOptions;
+    dark?: ColorsOptions;
+};
 
-export interface ColorsOptions extends GravityColorsOptions, YFMColorOptions {}
+export type ColorsOptions = GravityColorsOptions & YFMColorOptions;
 
 export const YFM_COLOR_KEYS = [
     'note-info-background',
     'note-tip-background',
     'note-warning-background',
     'note-important-background',
-    'hljs-addition', // нужно ли давать менять hljs переменные?
-];
+] as const;
 
 export type YFMColorOptions = {
     [K in (typeof YFM_COLOR_KEYS)[number]]?: string;
 };
 
-export interface GravityColorsOptions extends BrandDependColorOptions {
+export type GravityColorsOptions = BrandDependColorOptions & {
     'base-misc-light'?: string;
     'line-generic'?: string;
     'base-generic'?: string;
@@ -28,24 +32,32 @@ export interface GravityColorsOptions extends BrandDependColorOptions {
     'text-complementary'?: string;
     'text-hint'?: string;
     'text-misc'?: string;
-}
+};
 
-export interface BrandDependColorOptions {
-    'base-brand'?: string;
-    'base-background'?: string;
-    'base-brand-hover'?: string;
-    'base-selection'?: string;
-    'base-selection-hover'?: string;
-    'text-link'?: string;
-    'text-link-hover'?: string;
-    'text-brand'?: string;
-    'text-brand-heavy'?: string;
-    'line-brand'?: string;
-}
+export const BRAND_COLOR_KEYS = [
+    'base-brand',
+    'base-background',
+    'base-brand-hover',
+    'base-selection',
+    'base-selection-hover',
+    'text-link',
+    'text-link-hover',
+    'text-brand',
+    'text-brand-heavy',
+    'line-brand',
+] as const;
 
-export interface Theme extends Record<ThemeVariant, ThemeOptions> {}
+export type BrandDependColorOptions = {
+    [K in (typeof BRAND_COLOR_KEYS)[number]]?: string;
+};
+
+export type Theme = {
+    base?: ThemeOptions;
+    light?: ThemeOptions;
+    dark?: ThemeOptions;
+};
 
 export interface ThemeOptions {
-    palette: Record<string, string>;
+    palette?: Record<string, string>;
     colors: ColorsOptions;
 }
