a2a16282714dfd4688232282a4ccc0707e476dfc
3y3 <3y3@ya.ru>
2025-02-13 13:10:39 +0300

    deps: Update core deps

--- src/extensions/search/index.ts
+++ src/extensions/search/index.ts
@@ -1,5 +1,5 @@
 import type {BaseProgram, IExtension} from '@diplodoc/cli/lib/program';
-import type {ProviderConfig, Run as ProviderRun} from './provider';
+import type {ProviderConfig} from './provider';
 
 import {join} from 'node:path';
 import {getHooks as getBuildHooks} from '@diplodoc/cli/commands/build';
@@ -9,8 +9,6 @@ import {LocalSearchProvider} from './provider';
 
 const API_LINK = '_search/api.js';
 
-const SEARCH_API = require.resolve('@diplodoc/search-extension/worker');
-
 export class Extension implements IExtension {
     apply(program: BaseProgram) {
         getBuildHooks(program)
@@ -19,7 +17,7 @@ export class Extension implements IExtension {
                 getSearchHooks<ProviderConfig>(run.search)
                     .Provider.for('local')
                     .tap('LocalSearch', (_connector, config) => {
-                        return new LocalSearchProvider(run as ProviderRun, {
+                        return new LocalSearchProvider(run, {
                             ...config,
                             api: API_LINK,
                         });
@@ -30,7 +28,10 @@ export class Extension implements IExtension {
             .AfterRun.for('html')
             .tapPromise('LocalSearch', async (run) => {
                 if (run.search.enabled) {
-                    await run.copy(SEARCH_API, join(run.output, API_LINK));
+                    await run.copy(
+                        join(run.assetsPath, 'search-extension', 'api.js'),
+                        join(run.output, API_LINK),
+                    );
                 }
             });
     }
