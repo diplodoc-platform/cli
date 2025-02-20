cba9e686df3d8c0a4a42001ec2a3c9aa7c21ed87
3y3 <3y3@ya.ru>
2025-02-20 15:57:26 +0300

    fix: Extend out of scope logging

--- src/core/run/errors.ts
+++ src/core/run/errors.ts
@@ -3,10 +3,11 @@ export class InsecureAccessError extends Error {
 
     readonly realstack: AbsolutePath[];
 
-    constructor(file: AbsolutePath, stack?: AbsolutePath[]) {
+    constructor(file: AbsolutePath, stack?: AbsolutePath[], scopes?: AbsolutePath[]) {
         const message = [
             `Requested file '${file}' is out of project scope.`,
             stack && 'File resolution stack:\n\t' + stack.join('\n\t'),
+            scopes && 'Allowed scopes:\n\t' + scopes.join('\n\t'),
         ]
             .filter(Boolean)
             .join('\n');
