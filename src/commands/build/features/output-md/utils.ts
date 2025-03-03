edc671cc1b28f4585840e2545639db457ba058b9
3y3 <3y3@ya.ru>
2025-03-03 17:58:44 +0300

    fix: Fix Latex and Mermaid processing

--- src/commands/build/features/output-md/utils.ts
+++ src/commands/build/features/output-md/utils.ts
@@ -1,5 +1,8 @@
 import type {CollectPlugin} from '~/core/markdown';
 
+import * as mermaid from '@diplodoc/mermaid-extension';
+import * as latex from '@diplodoc/latex-extension';
+
 type Plugin = {
     collect?: CollectPlugin;
 };
@@ -8,7 +11,22 @@ type Plugin = {
 export function getCustomCollectPlugins(): CollectPlugin[] {
     try {
         const plugins: Plugin[] = require(require.resolve('./plugins'));
-        const collects = (Array.isArray(plugins) ? plugins : []).map((plugin) => plugin.collect);
+
+        const collects = [
+            mermaid.transform({
+                bundle: false,
+                runtime: '_bundle/mermaid-extension.js',
+            }),
+            latex.transform({
+                bundle: false,
+                runtime: {
+                    script: '_bundle/latex-extension.js',
+                    style: '_bundle/latex-extension.css',
+                },
+            }),
+        ]
+            .concat(plugins || [])
+            .map((plugin) => plugin.collect);
 
         return collects.filter(Boolean) as CollectPlugin[];
     } catch (e) {
