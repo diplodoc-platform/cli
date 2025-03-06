99f83f68ac2a4b4c2fb3688fc04f72b688b4833a
3y3 <3y3@ya.ru>
2025-03-07 00:56:27 +0300

    fix: Improve VarsService memory consumption

--- src/services/preset.ts
+++ src/services/preset.ts
@@ -1,43 +1,92 @@
-import {dirname, normalize} from 'path';
+import {dirname} from 'node:path';
 
 import {YfmPreset} from '../models';
-import {VarsService} from '~/core/vars';
+import {normalizePath, own} from '~/core/utils';
+import {uniq} from 'lodash';
+import {ArgvService} from '~/services/index';
 
-export type PresetStorage = Map<string, YfmPreset>;
+export type PresetStorage = Record<string, YfmPreset>;
 
-let presetStorage: PresetStorage = new Map();
+let presetStorage: PresetStorage = {};
 
-function init(vars: VarsService) {
-    for (const [path, values] of vars.entries) {
-        presetStorage.set(dirname(path), values);
-    }
+function init(presets: PresetStorage) {
+    presetStorage = presets;
 }
 
-function get(path: string): YfmPreset {
-    let vars = presetStorage.get(normalize(path));
-    while (!vars) {
-        path = dirname(path);
-        vars = presetStorage.get(normalize(path));
+function get(path: RelativePath): YfmPreset {
+    const scopes = _scopes(dirname(path));
 
-        if (path === '.') {
-            break;
+    return new Proxy(
+        {},
+        {
+            has(_target, prop: string) {
+                for (const scope of scopes) {
+                    if (own(scope, prop)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            },
+
+            get(_target, prop: string) {
+                for (const scope of scopes) {
+                    if (own(scope, prop)) {
+                        return scope[prop];
+                    }
+                }
+
+                return undefined;
+            },
+
+            getOwnPropertyDescriptor(_target, prop: string) {
+                for (const scope of scopes) {
+                    if (own(scope, prop)) {
+                        return {configurable: true, enumerable: true, value: scope[prop]};
+                    }
+                }
+
+                return undefined;
+            },
+
+            ownKeys() {
+                const keys = [];
+
+                for (const scope of scopes) {
+                    keys.push(...Object.keys(scope));
+                }
+
+                return uniq(keys);
+            },
+        },
+    );
+}
+
+function _scopes(path: RelativePath) {
+    const {vars, varsPreset} = ArgvService.getConfig();
+    const presets = [vars];
+
+    let dir = normalizePath(path);
+    do {
+        if (presetStorage[dir]) {
+            presets.push(presetStorage[dir][varsPreset]);
+            if (varsPreset !== 'default') {
+                presets.push(presetStorage[dir]['default']);
+            }
         }
-    }
 
-    return vars || {};
+        dir = normalizePath(dirname(dir));
+    } while (dir && dir !== '.');
+
+    return presets;
 }
 
-function getPresetStorage(): Map<string, YfmPreset> {
-    return presetStorage;
-}
-
-function setPresetStorage(preset: Map<string, YfmPreset>): void {
+function setPresetStorage(preset: Record<string, YfmPreset>): void {
     presetStorage = preset;
 }
 
 export default {
     init,
     get,
-    getPresetStorage,
     setPresetStorage,
 };
