73af4d67735978b5cbc909bb24f464c1cde6aed5
3y3 <3y3@ya.ru>
2025-03-09 22:13:57 +0300

    perf: Improve heading match performance

--- src/core/markdown/loader.ts
+++ src/core/markdown/loader.ts
@@ -189,34 +189,107 @@ function resolveAssets(this: LoaderContext, content: string) {
 
 function resolveHeadings(this: LoaderContext, content: string) {
     const headings = [];
-
-    const commonRx = '((^|\\n)(?<common>(#{1,6}\\s*).*?))(?=\\n|$)';
-    const alternateRx = '(?:^(\\r?\\n)*|(\\r?\\n){2,})(?<alternate>[\\s\\S]+?\\n(-+|=+))(?=\\n|$)';
-    const rx = new RegExp(`${commonRx}|${alternateRx}`, 'g');
+    const heading = /(?<=^|\n)([#=-])/g;
 
     let match;
     // eslint-disable-next-line no-cond-assign
-    while ((match = rx.exec(content))) {
-        const {common, alternate} = match!.groups!;
-        const heading = common || alternate;
+    while ((match = heading.exec(content))) {
+        const [result, location] = match[1] === '#'
+            ? findCommonHeading(match.index, content)
+            : findAlternateHeading(match.index, content);
 
-        headings.push({
-            content: heading,
-            location: [rx.lastIndex - heading.length, rx.lastIndex] as Location,
-        });
+        if (result) {
+            headings.push({content: result, location});
+            heading.lastIndex = location[1];
+        }
     }
-
-    // const {title, headings} = transform(raw.join('\n\n'), {
-    //     assetsPublicPath: './',
-    //     extractTitle: true,
-    //     needFlatListHeadings: true,
-    //     supportGithubAnchors,
-    //     log: this.logger,
-    // }).result;
 
     this.markdown.setHeadings(this.path, headings);
 
     return content;
+}
+
+const SPACE = /[ \t]/;
+
+function findCommonHeading(start: number, content: string): [string, Location] | never[] {
+    let title = '';
+
+    let index = start;
+    while (index < content.length) {
+        if (index - start > 6) {
+            return [];
+        }
+
+        if (content[index] === '#') {
+            index++;
+        } else if (content[index].match(SPACE)) {
+            break;
+        } else {
+            return [];
+        }
+    }
+
+    while (index < content.length) {
+        if (content[index + 1] && content[index] !== '\n') {
+            title += content[index];
+            index++;
+        } else if (title.trim()) {
+            return [content.slice(start, index), [start, index]];
+        } else {
+            return [];
+        }
+    }
+
+    return [];
+}
+
+function findAlternateHeading(start: number, content: string): [string, Location] | never[] {
+    const delim = content[start];
+    const newline = [];
+
+    let title = '';
+
+    let end = start;
+    while (end < content.length) {
+        if (content[end] === delim) {
+            end++;
+        } else if (content[end] === '\n' || !content[end + 1]) {
+            break;
+        } else {
+            return [];
+        }
+    }
+
+    let index = start - 1;
+    while (index >= 0) {
+        const isContentStart = !index;
+        const isTitleStart = newline.length === 2 && !content.slice(...newline).trim();
+
+        if (newline.length && !isTitleStart) {
+            newline.length = 1;
+        }
+
+        if (!isTitleStart) {
+            title = content[index] + title;
+        }
+
+        if (isContentStart || isTitleStart) {
+            const clean = title.trimStart();
+            if (clean) {
+                return [content.slice(start - clean.length, end), [start - clean.length, end]];
+            } else {
+                return [];
+            }
+        }
+
+        if (content[index] === '\n') {
+            newline.unshift(index);
+        }
+
+        index--;
+    }
+
+    return [];
 }
 
 function applyPlugins(this: LoaderContext, content: string) {
