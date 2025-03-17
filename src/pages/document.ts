44403fd83d40625a0ee218a9f07d57533647ffd9
Sergey Zolotonos <gold-serg@yandex-team.ru>
2025-03-17 16:25:00 +0300

    fixup! c997ec64910a0a7fd80d702ed2cdba14f959c113

--- src/pages/document.ts
+++ src/pages/document.ts
@@ -101,9 +101,12 @@ function getMetadata(metadata: VarsMetadata | undefined, restMeta: LeadingPage['
     if (metadata) {
         metadata.forEach(addMetaTagsFromObject);
     }
-    
+
     // Add generator meta tag with versions
-    addMetaTagsFromObject({name: 'generator', content: `Diplodoc Platform v${VERSION} (Transform v${VERSION_TRANSFORM})`});
+    addMetaTagsFromObject({
+        name: 'generator',
+        content: `Diplodoc Platform v${VERSION} (Transform v${VERSION_TRANSFORM})`,
+    });
 
     if (restMeta) {
         Object.entries(restMeta)
