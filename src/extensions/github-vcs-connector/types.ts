a35f71cd812df5bff32f22704ae4c4942d2025a3
3y3 <3y3@ya.ru>
2025-04-09 17:42:26 +0300

    feat: Improve vcs connector

--- src/extensions/github-vcs-connector/types.ts
+++ src/extensions/github-vcs-connector/types.ts
@@ -1,4 +1,9 @@
 import type {GitConfig} from './git-client';
 import type {GithubConfig} from './github-client';
 
-export type Config = GitConfig & GithubConfig;
+export type Config = GitConfig &
+    GithubConfig & {
+        mtimes: {enabled: true};
+        authors: {enabled: true};
+        contributors: {enabled: true};
+    };
