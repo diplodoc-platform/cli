58c1c3c323acfc36fbf4cb21d49a63ca79a8c592
Sergey Zolotonos <gold-serg@yandex-team.ru>
2025-04-21 16:12:49 +0300

    fix: Update page-constructor-schema and script, add e2e test

--- scripts/regenerate-schemas.js
+++ scripts/regenerate-schemas.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
+/* eslint-disable guard-for-in */
 
 // Replaces some fields in page constructor schema and saves it to schemas folder.
+// Add attributes to some fields in page constructor schema and saves it to schemas folder.
+// Extend scheme to custom part tree scheme
 
-const {writeFileSync} = require('fs');
-const {dump} = require('js-yaml');
+const {writeFileSync, readFileSync} = require('fs');
+const {dirname, join} = require('node:path');
+const {dump, load} = require('js-yaml');
 const {generateDefaultSchema} = require('@gravity-ui/page-constructor/schema/index.js');
+
+const ROOT = dirname(require.resolve('@diplodoc/cli/package'));
 
 function replaceField(obj, oldFieldName, newFieldName, newFieldValue) {
     if (Array.isArray(obj)) {
@@ -22,8 +28,50 @@ function replaceField(obj, oldFieldName, newFieldName, newFieldValue) {
     }
     return obj;
 }
+function extendField(obj, fieldNames, extObj) {
+  if (Array.isArray(obj)) {
+    return obj.map(item => extendField(item, fieldNames, extObj));
+  } else if (obj !== null && typeof obj === "object") {
+    const newObj = {};
+    for (const key in obj) {
+      if (fieldNames.includes(key)) {
+        newObj[key] = {
+          ...extendField(obj[key], fieldNames, extObj),
+          ...extObj,
+        };
+      } else {
+        newObj[key] = extendField(obj[key], fieldNames, extObj);
+      }
+    }
+    return newObj;
+  }
+  return obj;
+}
+function unionExtScheme(obj, extSchema) {
+  if (Array.isArray(obj)) {
+    return obj.map((item, index) => unionExtScheme(item, extSchema[index] || {}));
+  } else if (obj !== null && typeof obj === "object") {
+    const newObj = {...extSchema};
+    for (const key in obj) {
+      if (newObj[key] && typeof newObj[key] === "object" && !Array.isArray(newObj[key])) {
+        newObj[key] = {
+          ...newObj[key],
+          ...unionExtScheme(obj[key], extSchema[key]),
+        };
+      } else {
+        newObj[key] = unionExtScheme(obj[key], Array.isArray(newObj[key]) ? extSchema[key] : {});
+      }
+    }
+    return newObj;
+  }
+  return obj;
+}
 
 const schema = generateDefaultSchema();
-const modifiedSchema = replaceField(schema, 'contentType', 'translate', 'md');
+let modifiedSchema = replaceField(schema, 'contentType', 'translate', 'md');
+modifiedSchema = extendField(modifiedSchema, ['urlTitle', 'label'], {'translate': 'md'});
+
+const extScheme = load(readFileSync(join(ROOT, 'schemas/page-constructor-schema-extend.yaml'), 'utf8'));
+modifiedSchema = unionExtScheme(modifiedSchema, extScheme);
 
 writeFileSync('schemas/page-constructor-schema.yaml', dump(modifiedSchema));
--- tests/e2e/translation.spec.ts
+++ tests/e2e/translation.spec.ts
@@ -8,8 +8,18 @@ const generateMapTestTemplate = (testTitle: string, testRootPath: string, extend
     });
 }
 
-describe('Translate command', () => {
-    generateMapTestTemplate('filter files on extract', 'mocks/translation', 'translate extract translate extract --source ru-RU --target es-ES')
+const generateFilesYamlTestTemplate = (testTitle: string, testRootPath: string, extendedCommand: string) => {
+    test(testTitle, () => {
+        const {inputPath, outputPath} = getTestPaths(testRootPath);
+        runYfmDocs(inputPath, outputPath, {md2html: false, md2md: false, skipDefaults: true}, extendedCommand);
+        compareDirectories(outputPath);
+    });
+}
 
-    generateMapTestTemplate('filter files on extract with extra exclude option', 'mocks/translation', 'translate extract --source ru-RU --target es-ES --exclude ru/_no-translate/*.md')
+describe('Translate command', () => {
+    generateMapTestTemplate('filter files on extract', 'mocks/translation/dir-files', 'translate extract translate extract --source ru-RU --target es-ES')
+
+    generateMapTestTemplate('filter files on extract with extra exclude option', 'mocks/translation/dir-files', 'translate extract --source ru-RU --target es-ES --exclude ru/_no-translate/*.md')
+
+    generateFilesYamlTestTemplate('extract yaml scheme files', 'mocks/translation/yaml-scheme', 'translate extract --source ru-RU --target en-US')
 });
