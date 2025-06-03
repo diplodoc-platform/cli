cf276ea1b0b4aff856a2ff19fd920606beff64b1
3y3 <3y3@ya.ru>
2025-06-03 23:20:09 +0300

    fix: Disable VCS data processing on threads

--- src/core/vcs/types.ts
+++ src/core/vcs/types.ts
@@ -1,4 +1,12 @@
+export interface SyncData {
+    mtimes: Record<NormalizedPath, number>;
+    authors: Record<NormalizedPath, Contributor>;
+    contributors: Record<NormalizedPath, Contributor[]>;
+}
+
 export interface VcsConnector {
+    getData(): SyncData;
+    setData(data: SyncData): void;
     getUserByLogin(login: string): Promise<Contributor | null>;
     getAuthorByPath(path: RelativePath): Promise<Contributor | null>;
     getContributorsByPath(path: RelativePath, deps: RelativePath[]): Promise<Contributor[]>;
