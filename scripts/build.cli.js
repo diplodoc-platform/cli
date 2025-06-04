925854b226c154dae357021704f3cf782377ae40
3y3 <3y3@ya.ru>
2025-06-04 20:34:31 +0300

    feat: Rename github-vcs-connector to github-vcs

--- scripts/build.cli.js
+++ scripts/build.cli.js
@@ -92,7 +92,7 @@ const builds = [
 
 const extensions = [
     ['src/extensions/report-logs/index.ts', 'report-logs'],
-    ['src/extensions/github-vcs-connector/index.ts', 'github-vcs-connector'],
+    ['src/extensions/github-vcs/index.ts', 'github-vcs'],
 ];
 
 const libs = glob('./src/core/*/index.ts', {ignore: ['**/test/*']});
