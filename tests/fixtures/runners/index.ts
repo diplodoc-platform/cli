8f0e168cedc2d267dcf4250a05e0a608344dd789
3y3 <3y3@ya.ru>
2025-06-11 14:12:07 +0300

    chore: Use binary runner for tests
    fix

--- tests/fixtures/runners/index.ts
+++ tests/fixtures/runners/index.ts
@@ -1,3 +1,5 @@
+import {resolve} from 'node:path';
+
 import {SourceRunner} from './source';
 import {BinaryRunner} from './binary';
 import {Runner} from './types';
@@ -5,7 +7,7 @@ import {Runner} from './types';
 export {Runner};
 
 export function createRunner(): Runner {
-    const binaryPath = process.env.DIPLODOC_BINARY_PATH;
+    const binaryPath = process.env.DIPLODOC_BINARY_PATH || resolve(__dirname, '../../../build/index.js');
 
     if (binaryPath) {
         return new BinaryRunner(binaryPath);
