97696c8c7511b0dbb48510de165936c374573677
separatrix <nikitalochmatov@gmail.com>
2025-08-06 22:46:22 +0300

    fix: fixed prettier

--- src/constants.ts
+++ src/constants.ts
@@ -58,9 +58,6 @@ export enum ResourceType {
     csp = 'csp',
 }
 
-export const testedMetaFields = [
-    'interface',
-    'resources',
-];
+export const testedMetaFields = ['interface', 'resources'];
 
 export const PAGE_PROCESS_CONCURRENCY = Number(process.env.PAGE_PROCESS_CONCURRENCY) || 300;
