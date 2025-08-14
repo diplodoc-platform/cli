d8be8408fae79acac8bbe54f67caeb8980b27dc6
3y3 <3y3@ya.ru>
2025-08-14 13:26:34 +0300

    feat: Add arcadia-vcs extension

---
+++ src/extensions/arcadia-vcs/types.ts
@@ -0,0 +1,34 @@
+export interface LogConfig {
+    vcs: {
+        scopes: string[];
+        initialCommit?: string;
+    };
+    authors?: {
+        ignore?: string[];
+    };
+    contributors?: {
+        ignore?: string[];
+    };
+}
+
+export type Config = {
+    mtimes: {enabled: true};
+    authors: {
+        enabled: true;
+        ignore?: string[];
+    };
+    contributors: {
+        enabled: true;
+        ignore?: string[];
+    };
+    vcs: {
+        enabled: boolean;
+        scopes: string[];
+        initialCommit?: string;
+    };
+};
+
+export type Args = {
+    vcsInitialCommit: string;
+    vcsScopes: string[];
+};
