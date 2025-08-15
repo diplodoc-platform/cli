e2ce3e51666c45d2fdf20c97b51d58ed0a386f79
3y3 <3y3@ya.ru>
2025-08-15 13:05:28 +0300

    feat: Add required `getBase` method to VCSConnector interface

--- src/core/vcs/connector.ts
+++ src/core/vcs/connector.ts
@@ -22,6 +22,10 @@ export class DefaultVcsConnector implements VcsConnector {
 
     setData() {}
 
+    async getBase() {
+        return '.' as RelativePath;
+    }
+
     async getContributorsByPath() {
         return [];
     }
