839d4082011fb305cac1c1b5be63500aee435004
Sergey Zolotonos <gold-serg@yandex-team.ru>
2025-08-15 13:29:22 +0300

    fix: add flat to deps in contributers

--- src/commands/build/features/contributors/index.ts
+++ src/commands/build/features/contributors/index.ts
@@ -6,10 +6,11 @@ import {uniq} from 'lodash';
 
 import {getHooks as getBaseHooks} from '~/core/program';
 import {getHooks as getLeadingHooks} from '~/core/leading';
-import {getHooks as getMarkdownHooks} from '~/core/markdown';
+import {IncludeInfo, getHooks as getMarkdownHooks} from '~/core/markdown';
 import {defined, toggleable} from '~/core/config';
 
 import {options} from './config';
+import {flat} from '~/core/utils';
 
 export type ContributorsArgs = {
     mtimes?: {enabled: boolean};
@@ -59,7 +60,7 @@ export class Contributors {
             getMarkdownHooks(run.markdown).Dump.tapPromise(
                 {name: 'Contributors', stage: -1},
                 async (vfile) => {
-                    const rawDeps = await run.markdown.deps(vfile.path);
+                    const rawDeps = flat<IncludeInfo>(await run.markdown.deps(vfile.path));
                     const deps = uniq(rawDeps.map(({path}) => path));
                     const meta = await run.vcs.metadata(vfile.path, run.meta.get(vfile.path), deps);
 
