7a33444b186ce59ff61363c6ddf4277cd6a6ca1c
3y3 <3y3@ya.ru>
2025-08-15 13:07:11 +0300

    fix

--- src/core/vcs/VcsService.ts
+++ src/core/vcs/VcsService.ts
@@ -103,6 +103,11 @@ export class VcsService implements VcsConnector {
         return result;
     }
 
+    @memoize()
+    async getBase() {
+        return this.connector.getBase();
+    }
+
     async getContributorsByPath(
         path: RelativePath,
         deps: RelativePath[] = [],
