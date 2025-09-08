f0158bdbca00b89675e1a0eeceadd10d66884420
separatrix <nikitalochmatov@gmail.com>
2025-09-09 00:18:55 +0300

    fix: headings processing

--- src/commands/build/features/skip-html/index.ts
+++ src/commands/build/features/skip-html/index.ts
@@ -4,13 +4,15 @@ import {getHooks as getTocHooks} from '~/core/toc';
 import {getHooks as getMarkdownHooks} from '~/core/markdown';
 import {getHooks as getLeadingHooks} from '~/core/leading';
 import {getHooks as getBaseHooks} from '~/core/program';
+import {getHooks as getEntryHooks} from '../../services/entry';
 import {normalizePath, own} from '~/core/utils';
 import {Command, defined} from '~/core/config';
 import {options} from './config';
 import type {Toc, TocItem} from '~/core/toc';
-import {getHref} from './utils';
+import {getHref, getStateData} from './utils';
 import {join} from 'node:path';
 import skipHtmlLinks from './plugins/skipHtmlLinks';
+import {Template} from '~/core/template';
 
 export type SkipHtmlArgs = {
     skipHtmlExtension: boolean;
@@ -19,6 +21,8 @@ export type SkipHtmlArgs = {
 export type SkipHtmlConfig = {
     skipHtmlExtension: boolean;
 };
+
+const __Entry__ = Symbol('isEntry');
 
 export class SkipHtml {
     apply(program: Build) {
@@ -48,6 +52,22 @@ export class SkipHtml {
 
                         return item;
                     });
+                });
+
+                // Transform any entry to final html page without .html
+                getEntryHooks(run.entry).Dump.tapPromise('SkipHtml', async (vfile) => {
+                    const toc = await run.toc.dump(vfile.path);
+                
+                    const data = getStateData(vfile.data);
+
+                    data.toc = toc.data;
+
+                    const state = await run.entry.state(vfile.path, data);
+                    const template = new Template(vfile.path, state.lang, [__Entry__]);
+                
+                    const html = await run.entry.page(template, state, toc.data);
+                    vfile.format(() => html);
+                    Object.assign(vfile.info, data);
                 });
 
                 // Connecting a plugin to bypass links
--- src/commands/build/features/skip-html/utils.ts
+++ src/commands/build/features/skip-html/utils.ts
@@ -1,5 +1,6 @@
 import type Token from 'markdown-it/lib/token';
 import type StateCore from 'markdown-it/lib/rules_core/state_core';
+import type {EntryData, PageData} from '~/commands/build';
 
 import url from 'url';
 import {filterTokens, isExternalHref, prettifyLink} from '~/core/utils';
@@ -38,4 +39,42 @@ export function getHref(href: string) {
     }
 
     return prettifyLink(href);
+}
+
+function mapHeadings(headings: any) {
+    return headings.map((heading: any) => {
+        const newHeading = { ...heading };
+
+        if (newHeading.href) {
+            newHeading.href = prettifyLink(newHeading.href);
+        }
+
+        if (Array.isArray(newHeading.items)) {
+            newHeading.items = mapHeadings(newHeading.items);
+        }
+
+        return newHeading;
+    });
+}
+
+
+export function getStateData(entry: EntryData): PageData {
+    if (entry.type === 'yaml') {
+        return {
+            leading: true,
+            data: entry.content.data,
+            meta: entry.meta,
+            title: entry.content.data.title || entry.meta.title || '',
+        };
+    } else {
+        const prettifiedHeadings = mapHeadings(entry.info.headings);
+        
+        return {
+            leading: false,
+            html: entry.content.toString(),
+            meta: entry.meta,
+            headings: prettifiedHeadings,
+            title: entry.info.title || entry.meta.title || '',
+        };
+    }
 }
