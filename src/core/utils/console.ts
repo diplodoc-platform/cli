c4892b9963f9f1bca06ebe71440fd4134415cfb4
3y3 <3y3@ya.ru>
2025-10-10 11:59:10 +0300

    chore: Extend MutableConsole by Console class

--- src/core/utils/console.ts
+++ src/core/utils/console.ts
@@ -1,37 +1,29 @@
-/* eslint-disable no-console */
+import {Console} from 'node:console';
 
-class MutableConsole {
+class MutableConsole extends Console {
     log(...args: unknown[]) {
         this.write('log', args);
     }
 
-    warn(...args: unknown[]) {
-        console.warn(...args);
-    }
-
-    error(...args: unknown[]) {
-        console.error(...args);
-    }
-
     time(label: string) {
         if (process.env.NODE_ENV !== 'test') {
-            console.time(label);
+            super.time(label);
         }
     }
 
     timeEnd(label: string) {
         if (process.env.NODE_ENV !== 'test') {
-            console.timeEnd(label);
+            super.timeEnd(label);
         }
     }
 
     private write(level: 'log' | 'error' | 'warn', args: unknown[]) {
         if (process.env.NODE_ENV !== 'test') {
-            console[level](...args);
+            super[level](...args);
         }
     }
 }
 
-const _console = new MutableConsole();
+const _console = new MutableConsole(process.stdout, process.stderr, false);
 
 export {_console as console};
