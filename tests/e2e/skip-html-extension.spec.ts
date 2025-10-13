dfc4847fc5a7ab1273812a773c088025aaf0b0b2
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2025-10-13 16:44:11 +0300

    fix: fixed redirects file (#1461)
    * fix: fixed redirects file
    
    * fix: updated tests

--- tests/e2e/skip-html-extension.spec.ts
+++ tests/e2e/skip-html-extension.spec.ts
@@ -1,10 +1,10 @@
-import {describe, it} from 'vitest';
+import {describe, test} from 'vitest';
 
 import {TestAdapter, compareDirectories, getTestPaths} from '../fixtures';
 
-describe('Skip html extension', () => {
-    it('transforms links correctly', async () => {
-        const {inputPath, outputPath} = getTestPaths('mocks/skip-html-extension');
+const generateMapTestTemplate = (testTitle: string, testRootPath: string) => {
+    test(testTitle, async () => {
+        const {inputPath, outputPath} = getTestPaths(testRootPath);
 
         await TestAdapter.testBuildPass(inputPath, outputPath, {
             md2md: false,
@@ -13,4 +13,16 @@ describe('Skip html extension', () => {
         });
         await compareDirectories(outputPath);
     });
+};
+
+describe('Skip html extension', () => {
+    generateMapTestTemplate(
+        'correctly trims .html and index.html on multilingual docs',
+        'mocks/skip-html-extension/multilingual',
+    );
+
+    generateMapTestTemplate(
+        'correctly trims .html and index.html on monolingual docs',
+        'mocks/skip-html-extension/monolingual',
+    );
 });
