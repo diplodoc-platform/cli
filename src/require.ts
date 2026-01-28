fd66dbe9218fc5362a749b553fac26049e9650fe
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2026-01-28 21:15:21 +0300

    Update require.ts

--- src/require.ts
+++ src/require.ts
@@ -1,6 +1,6 @@
 import Module from 'node:module';
 import {resolve} from 'node:path';
-
+// test
 Module.prototype.require = ((original, map) =>
     Object.assign(function (this: Module, name: string) {
         return original.call(this, map[name] || name);
