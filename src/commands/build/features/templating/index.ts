c6956a5e81764eb81753f11b89e814a192c95419
Anton Vikulov <rndnm@yandex-team.ru>
2026-01-29 12:47:29 +0300

    Drop empty presets files

--- src/commands/build/features/templating/index.ts
+++ src/commands/build/features/templating/index.ts
@@ -1,6 +1,9 @@
 import type {Build} from '~/commands/build';
+import {getHooks as getBuildHooks} from '~/commands/build';
 import type {Command} from '~/core/config';
+import {defined, valuable} from '~/core/config';
 import type {Preset} from '~/core/vars';
+import {getHooks as getVarsHooks} from '~/core/vars';
 import type {Run} from '~/commands/build/run';
 
 import {dirname, join} from 'node:path';
@@ -8,9 +11,6 @@ import {dump} from 'js-yaml';
 import {merge} from 'lodash';
 
 import {getHooks as getBaseHooks} from '~/core/program';
-import {getHooks as getBuildHooks} from '~/commands/build';
-import {getHooks as getVarsHooks} from '~/core/vars';
-import {defined, valuable} from '~/core/config';
 
 import {options} from './config';
 
@@ -122,13 +122,16 @@ export class Templating {
                                 {},
                             );
 
-                            await run.write(
-                                join(run.output, path),
-                                dump(result, {
-                                    lineWidth: 120,
-                                }),
-                                true,
-                            );
+                            const yaml = dump(result, {
+                                lineWidth: 120,
+                            });
+                            if (yaml === '{}') {
+                                await run.write(
+                                    join(run.output, path),
+                                    yaml,
+                                    true,
+                                );
+                            }
 
                             return presets;
                         },
