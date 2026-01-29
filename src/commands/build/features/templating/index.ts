bd6abe946067cb4650a6ceb289912f0d82187d16
Anton Vikulov <rndnm@yandex-team.ru>
2026-01-29 12:59:18 +0300

    fix

--- src/commands/build/features/templating/index.ts
+++ src/commands/build/features/templating/index.ts
@@ -126,7 +126,7 @@ export class Templating {
                             const yaml = dump(result, {
                                 lineWidth: 120,
                             });
-                            if (yaml === '{}') {
+                            if (yaml !== '{}') {
                                 await run.write(join(run.output, path), yaml, true);
                             }
 
