1590d8ecc9f2343f0e7c7403b66e560d2cede41a
yurok8 <yurok8@yandex-team.ru>
2026-02-04 21:29:59 +0300

    feat: add error if duplicated key

--- src/core/markdown/loader/mangle-frontmatter.ts
+++ src/core/markdown/loader/mangle-frontmatter.ts
@@ -2,8 +2,42 @@ import type {LoaderContext} from '../loader';
 
 import {extractFrontMatter, liquidJson} from '@diplodoc/liquid';
 
+type YamlErrorMark = {
+    line?: number;
+};
+
+type YamlError = {
+    name?: string;
+    reason?: string;
+    mark?: YamlErrorMark;
+};
+
 function safeExtractFrontmatter(this: LoaderContext, content: string) {
-    return extractFrontMatter(content, {json: true});
+    try {
+        return extractFrontMatter(content);
+    } catch (error) {
+        const err = error as YamlError;
+        if (err.reason === 'duplicated mapping key') {
+            const err = error as YamlError;
+            const path = this.path;
+
+            const reason = err.reason || 'invalid front matter format';
+            const line = typeof err.mark?.line === 'number' ? Number(err.mark.line) + 1 : undefined;
+
+            const context =
+                line === undefined
+                    ? `[Reason: "${reason}"]`
+                    : `[Reason: "${reason}"; Line: ${line}]`;
+
+            const errorMessage =
+                line === undefined
+                    ? `${path}: ${err.name} / invalid front matter format ${context}`
+                    : `${path}: ${line}: ${err.name} / invalid front matter format ${context}`;
+
+            this.logger.error(errorMessage);
+        }
+        return extractFrontMatter(content, {json: true});
+    }
 }
 
 export function mangleFrontMatter(this: LoaderContext, rawContent: string) {
