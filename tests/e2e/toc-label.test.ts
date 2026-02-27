1e2cd05604a31fffdee345d849a1ffe449566a39
separatrix <nikitalochmatov@gmail.com>
2026-02-27 16:44:49 +0300

    fix: updated toc-label test

--- tests/e2e/toc-label.test.ts
+++ tests/e2e/toc-label.test.ts
@@ -7,9 +7,17 @@ describe('Toc label', () => {
         const {inputPath, outputPath} = getTestPaths('mocks/toc-label');
 
         await TestAdapter.testBuildPass(inputPath, outputPath, {
+            md2md: true,
+            md2html: false,
+            args: '-j2',
+        });
+        await TestAdapter.testBuildPass(outputPath, outputPath + '-html', {
             md2md: false,
             md2html: true,
+            args: '-j2',
         });
+
         await compareDirectories(outputPath);
+        await compareDirectories(outputPath + '-html');
     });
 });
