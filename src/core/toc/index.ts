57a22e2e0d5fccbffc83b1b243f6b55106796793
separatrix <nikitalochmatov@gmail.com>
2026-03-26 20:21:34 +0300

    chore: test

--- src/core/toc/index.ts
+++ src/core/toc/index.ts
@@ -1,6 +1,6 @@
 export type * from './types';
 export type {LoaderContext} from './loader';
-
+// в
 export {getHooks} from './hooks';
 export {IncludeMode} from './loader';
 export {TocService} from './TocService';
